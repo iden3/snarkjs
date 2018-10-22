5c514a897bc88887ead428a1e3f9bd7d528b61b1
Jordi Baylina <jordi@baylina.cat>
2018-10-22 08:34:49 +0200

    info and print constraints added

--- src/circuit.js
+++ src/circuit.js
@@ -93,22 +93,36 @@ module.exports = class Circuit {
         const lc2str = (lc) => {
             let S = "";
             for (let k in lc) {
-                const name = this.signals[k].names[0];
+                let name = this.signals[k].names[0];
+                if (name == "one") name = "";
                 let v = bigInt(lc[k]);
                 let vs;
                 if (!v.lesserOrEquals(__P__.shr(bigInt(1)))) {
                     v = __P__.sub(v);
                     vs = "-"+v.toString();
                 } else {
-                    vs = "+"+v.toString();
+                    if (S!="") {
+                        vs = "+"+v.toString();
+                    } else {
+                        vs = "";
+                    }
+                    if (vs!="1") {
+                        vs = vs + v.toString();;
+                    }
                 }
 
                 S= S + " " + vs + name;
             }
             return S;
-        }
-        const S = `[ ${lc2str(c[0])} ] * [ ${lc2str(c[1])} ] - [ ${lc2str(c[2])} ]`;
+        };
+        const S = `[ ${lc2str(c[0])} ] * [ ${lc2str(c[1])} ] - [ ${lc2str(c[2])} ] = 0`;
         console.log(S);
+    }
+
+    printConstraints() {
+        for (let i=0; i<this.constraints.length; i++) {
+            this.printCostraint(this.constraints[i]);
+        }
     }
 
     getSignalIdx(name) {
