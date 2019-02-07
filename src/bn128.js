d5d2aa859962e46b35aa573e8256773f6df88a7c
stanislasdrg@gmail.com <stanislasdrg@gmail.com>
2019-02-07 22:04:47 +0000

    Removed useless commas

--- src/bn128.js
+++ src/bn128.js
@@ -200,7 +200,7 @@ class BN128 {
                 f,
                 c.ell_0,
                 this.F2.mulScalar(c.ell_VW , pre1.PY),
-                this.F2.mulScalar(c.ell_VV , pre1.PX, ));
+                this.F2.mulScalar(c.ell_VV , pre1.PX));
 
             if (bit)
             {
@@ -208,8 +208,8 @@ class BN128 {
                 f = this._mul_by_024(
                     f,
                     c.ell_0,
-                    this.F2.mulScalar(c.ell_VW, pre1.PY, ),
-                    this.F2.mulScalar(c.ell_VV, pre1.PX, ));
+                    this.F2.mulScalar(c.ell_VW, pre1.PY),
+                    this.F2.mulScalar(c.ell_VV, pre1.PX));
             }
 
         }
@@ -230,7 +230,7 @@ class BN128 {
         f = this._mul_by_024(
             f,
             c.ell_0,
-            this.F2.mulScalar(c.ell_VW, pre1.PY, ),
+            this.F2.mulScalar(c.ell_VW, pre1.PY),
             this.F2.mulScalar(c.ell_VV, pre1.PX));
 
         return f;
