f4786f41308f56d0a2dd9269ad29ccb211b63d1d
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2022-03-26 12:51:26 -0700

    conditional compilation for SES compatibility

--- config/rollup.iife.config.js
+++ config/rollup.iife.config.js
@@ -3,6 +3,7 @@ import commonJS from "@rollup/plugin-commonjs";
 import virtual from "@rollup/plugin-virtual";
 import replace from "@rollup/plugin-replace";
 import visualizer from "rollup-plugin-visualizer";
+import jscc from 'rollup-plugin-jscc'
 
 const empty = "export default {}";
 
@@ -37,5 +38,8 @@ export default {
             "process.browser": !!process.env.BROWSER
         }),
         visualizer(),
+        jscc({
+            values: { _SES: process.env.SES },
+        }),
     ]
 };
