c7e24059da2443242e08035ae5d357e6a998b5e3
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2022-03-29 13:45:42 -0700

    pass in singleThread to ffjavascript

--- config/rollup.ses.config.js
+++ config/rollup.ses.config.js
@@ -2,6 +2,7 @@ import config from './rollup.iife.config';
 import fs from "fs";
 import { builtinModules as builtin } from "module";
 import jscc from 'rollup-plugin-jscc';
+import replace from "@rollup/plugin-replace";
 
 const pkg = JSON.parse(fs.readFileSync("./package.json"));
 delete pkg.dependencies["ejs"];
@@ -20,6 +21,11 @@ export default {
         ...config.plugins,
         jscc({
             values: { _SES: process.env.SES },
+        }),
+        replace({
+            // To silence to warning, the current default is false, but they are changing it next version
+            preventAssignment: false,
+            "process.ses": !!process.env.SES
         }),
     ]
 };
