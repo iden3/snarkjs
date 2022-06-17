02e3f1cb53160a09492fc3d591445c7eb4581d5e
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-06-17 13:39:25 -0700

    fix: Also delete the curve property

--- src/zkey_export_json.js
+++ src/zkey_export_json.js
@@ -4,6 +4,7 @@ import { utils } from "ffjavascript";
 export default async function zkeyExportJson(zkeyFileName) {
 
     const zKey = await readZKey(zkeyFileName, true);
+    delete zKey.curve;
     delete zKey.F;
 
     return utils.stringifyBigInts(zKey);
