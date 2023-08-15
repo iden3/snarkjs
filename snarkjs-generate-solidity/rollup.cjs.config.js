0546df7912e6df579e1001b82ea0e0b734047c37
Blaine Bublitz <blaine.bublitz@gmail.com>
2023-08-14 23:14:02 -0700

    build to root to avoid template path differences

--- snarkjs-generate-solidity/rollup.cjs.config.js
+++ snarkjs-generate-solidity/rollup.cjs.config.js
@@ -6,7 +6,7 @@ const pkg = JSON.parse(fs.readFileSync("./package.json"));
 export default {
     input: "main.js",
     output: {
-        file: "build/main.cjs",
+        file: "main.cjs",
         format: "cjs",
     },
     external: [
